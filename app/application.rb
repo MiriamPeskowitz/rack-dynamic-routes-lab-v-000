6c8b4ee8e0da0abd09094e2352fc35de2ce65d2c
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-10 01:50:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item.name = req.path.split("/items/").last
+      @@item.name = req.path.split("/items/").last
         if item =@@items.find do |item|
           item.name == item_name
           resp.write item.price
