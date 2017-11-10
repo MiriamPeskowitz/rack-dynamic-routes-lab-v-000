0b019271f6e9e37a31f3dfd57376d4e735189ffe
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-10 01:51:46 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,52 +1,30 @@
-# # Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
-# # If a user requests /items/<Item Name> it should return the price of that item
-# # IF a user requests an item that you don't have, then return a 400 and an error message
-# class Application
-#
-#   @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
-#
-#   def call(env)
-#     resp = Rack::Response.new
-#     req = Rack::Request.new(env)
-#
-#     if req.path.match(/items/)
-#       item.name = req.path.split("/items/").last
-#         if item =@@items.find do |item|
-#           item.name == item_name
-#           resp.write item.price
-#           end
-#         else
-#         resp.status = 400
-#         resp.write "Item not found"
-#         end
-#     else
-#       resp.status = 404
-#       resp.write "Route not found"
-#     end
-#     resp.finish
-#   end
-# end
-
+# Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
+# If a user requests /items/<Item Name> it should return the price of that item
+# IF a user requests an item that you don't have, then return a 400 and an error message
 class Application
 
-  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+  @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last
-      if item =@@items.find{|i| i.name == item_name}
-        resp.write item.price
-      else
+      item.name = req.path.split("/items/").last
+        if item =@@items.find do |item|
+          item.name == item_name
+          resp.write item.price
+          end
+        else
         resp.status = 400
         resp.write "Item not found"
-      end
+        end
     else
-      resp.status=404
+      resp.status = 404
       resp.write "Route not found"
     end
     resp.finish
   end
-
 end
+
+
