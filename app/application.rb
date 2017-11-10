661e9daed88ab6ffaf6e9628a51ad75fb9551ad3
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-10 01:51:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,13 @@
 # # If a user requests /items/<Item Name> it should return the price of that item
 # # IF a user requests an item that you don't have, then return a 400 and an error message
 # class Application
-# 
+#
 #   @@items = [Item.new("Figs",3.42), Item.new("Pears",0.99)]
-# 
+#
 #   def call(env)
 #     resp = Rack::Response.new
 #     req = Rack::Request.new(env)
-# 
+#
 #     if req.path.match(/items/)
 #       item.name = req.path.split("/items/").last
 #         if item =@@items.find do |item|
@@ -38,7 +38,7 @@ class Application
       item_name = req.path.split("/items/").last
       if item =@@items.find{|i| i.name == item_name}
         resp.write item.price
-      else 
+      else
         resp.status = 400
         resp.write "Item not found"
       end
