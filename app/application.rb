f99837924fcb0f4123d101c589ec28e4f6e18c37
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-10 01:53:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,8 +11,8 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-        if item =@@items.find do |item|
-          item.name == item_name
+        if item =@@items.find do |i|
+          i.name == item_name
           resp.write item.price
           end
         else
